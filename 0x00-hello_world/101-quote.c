cd3feea860278f3dbd7793d8db45874d6524c8f0
Uti James <uti.james09@gmail.com>
2023-02-19 16:12:34 +0000

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" 
-			- Dora Korpar, 2015-10-19\n", 59);
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDOUT_FILENO, msg, 59);
 	return (1);
 }
