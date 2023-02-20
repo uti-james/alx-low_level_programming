35ab84d444f8c9054c55a03a4ee0f1c91f61dda7
Uti James <uti.james09@gmail.com>
2023-02-20 23:11:28 +0000

    a program that prints the alphabet in lowercase, followed by a new lin

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
-		if(letter != 'e' && letter != 'q')
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'e' && letter != 'q')
 		{
 			putchar(letter);
 		}
