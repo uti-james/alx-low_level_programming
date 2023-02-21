107b372ee93388afe7a74320dec11051de6c32ce
Uti James <uti.james09@gmail.com>
2023-02-21 01:20:10 +0000

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,15 +4,18 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main() {
-    int i;
-    for (i = 0; i < 16; i++) {
-        if (i < 10) {
-            putchar(i + '0');
-        } else {
-            putchar(i - 10 + 'a');
-        }
-    }
-    putchar('\n');
-    return 0;
+int main(void)
+{
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10) {
+			putchar(i + '0');
+		} else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return 0;
 }
