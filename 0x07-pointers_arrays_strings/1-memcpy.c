8274b6661acd8270fe8d154ececd0b050fe79564
Uti James <uti.james09@gmail.com>
2023-03-07 12:23:47 +0000

    a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _memcpy - Copies n bytes from memory area src to memory area dest
  * @dest: Pointer to the destination array where the content is to be copied
@@ -12,7 +14,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{		
 		dest[i] = src[i];
+	}
 
 	return (dest);
 }
