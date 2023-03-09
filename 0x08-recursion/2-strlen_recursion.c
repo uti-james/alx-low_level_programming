970e4f739690cbdf17c4a0a58528f63515899260
Uti James <uti.james09@gmail.com>
2023-03-09 17:15:41 +0000

    a function that returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0') /* base case */
-        return 0;
-    else /* recursive case */
-        return 1 + _strlen_recursion(s + 1);
-}
+	if (*s == '\0')
+	{
+		return 0;
+	}
+		else
+		{
+			return 1 + _strlen_recursion(s + 1);
+		}
+	}
