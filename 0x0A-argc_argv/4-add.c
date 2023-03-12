94a69e05b47714796cb22b4510f9240406d3db3a
Uti James <uti.james09@gmail.com>
2023-03-12 03:12:35 +0000

    a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,23 +8,29 @@
  * @argv: array pointing to arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	int i, sum = 0;
 
-	if (argc < 1)
-		return (0);
+int main(int argc, char *argv[]) {
+	int sum = 0;
+	int i, j;
 
-	for (i = 1; i < argc; i++)
+	if (argc == 1) {
+		printf("0\n");
+		return 0;
+	}
+
+	for (i = 1; i < argc; i++) 
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++) 
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9') 
+			{
+				printf("Error\n");
+				return 1;
+			}
 		}
 		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
 
-	return (0);
+	printf("%d\n", sum);
+	return 0;
 }
