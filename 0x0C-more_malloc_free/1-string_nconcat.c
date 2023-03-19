b37641efb013830dd1dc7e377331ba2db1928a03
Uti James <uti.james09@gmail.com>
2023-03-19 13:59:21 +0000

    Added parenthesis to return statements to confide to Betty Style Guide

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,39 +15,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *result;
 
-	/* Handle NULL strings */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	/* Calculate length of s1 */
 	while (s1[len1] != '\0')
 		len1++;
 
-	/* Calculate length of s2 */
 	while (s2[len2] != '\0')
 		len2++;
 
-	/* Limit n to length of s2 */
 	if (n >= len2)
 		n = len2;
 
-	/* Allocate memory for concatenated string */
 	result = malloc(sizeof(char) * (len1 + n + 1));
 	if (result == NULL)
-		return NULL;
+		return (NULL);
 
-	/* Copy s1 into result */
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
-	/* Concatenate n bytes of s2 into result */
 	for (j = 0; j < n; j++)
 		result[i + j] = s2[j];
 
-	/* Add null terminator to result */
 	result[i + j] = '\0';
 
-	return result;
+	return (result);
 }
