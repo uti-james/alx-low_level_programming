79d1e88f1dde1d358c7392b254ac07d45547d920
Uti James <uti.james09@gmail.com>
2023-03-20 23:09:52 +0000

    Rewrite solution to pass betty style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,35 +2,27 @@
 #include "main.h"
 
 /**
- * array_range - creates an array of integers from min (included) to max
- * (included), ordered from min to max.
- * @min: the minimum integer to include in the array
- * @max: the maximum integer to include in the array
+ * array_range - creates an array of integers
+ * @min: the minimum value to include in the array
+ * @max: the maximum value to include in the array
  *
- * Return: pointer to the newly created array, or NULL on failure
+ * Return: pointer to the newly created array
+ *         NULL if min > max or if malloc fails
  */
-
 int *array_range(int min, int max)
 {
 	int *arr, i;
-	int arr_size = (max - min) + 1;
 
-	if (min >= max)
-	{
+	if (min > max)
 		return (NULL);
-	}
 
-	arr = malloc(arr_size * sizeof(int));
-
-	if (!arr)
-	{
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < arr_size; i++)
-	{
-		arr[i] = min++;
-	}
+	for (i = 0; i <= max - min; i++)
+		arr[i] = min + i;
 
 	return (arr);
 }
+
