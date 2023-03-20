6a39434adb2c496aa78160092bc4e91c555ac4fd
Uti James <uti.james09@gmail.com>
2023-03-20 15:43:26 +0000

    compiled the 2-main.c and 2-calloc.c files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _calloc - Allocates memory for an array using malloc
  * @nmemb: Number of elements to allocate
  * @size: Size of each element
  *
- * Return: Pointer to the allocated memory or NULL if 
- * 	   nmemb or size is 0 or if malloc fails
+ * Return: Pointer to the allocated memory or NULL if
+ *		nmemb or size is 0 or if malloc fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
