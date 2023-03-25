51d309c1fee6c22602b6f268fc5796de6bc6bb14
Uti James <uti.james09@gmail.com>
2023-03-25 02:23:36 +0000

    Added some indentations to make the code more readble

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,28 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
-    int bytes, i;
-    char *main_ptr = (char *)main; // get the pointer to the beginning of main function
+	int bytes, i;
+	char *main_ptr = (char *)main; // get the pointer to the beginning of main function
 
-    if (argc != 2) // check if correct number of arguments is passed
-    {
-        printf("Error\n"); // print error message
-        return 1; // return 1 to indicate error
-    }
+	if (argc != 2) // check if correct number of arguments is passed
+	{
+		printf("Error\n"); // print error message
+		return 1; // return 1 to indicate error
+	}
 
-    bytes = atoi(argv[1]); // convert the argument to integer
+	bytes = atoi(argv[1]); // convert the argument to integer
 
-    if (bytes < 0) // check if number of bytes is negative
-    {
-        printf("Error\n"); // print error message
-        return 2; // return 2 to indicate error
-    }
+	if (bytes < 0) // check if number of bytes is negative
+	{
+		printf("Error\n"); // print error message
+		return 2; // return 2 to indicate error
+	}
 
-    for (i = 0; i < bytes - 1; i++) // loop through the opcodes
-    {
-        printf("%02hhx ", *(main_ptr + i)); // print the opcode in hexadecimal format
-    }
-    printf("%02hhx\n", *(main_ptr + i)); // print the last opcode and newline
+	for (i = 0; i < bytes - 1; i++) // loop through the opcodes
+	{
+		printf("%02hhx ", *(main_ptr + i)); // print the opcode in hexadecimal format
+	}
+	printf("%02hhx\n", *(main_ptr + i)); // print the last opcode and newline
 
-    return 0; // return 0 to indicate success
+	return 0; // return 0 to indicate success
 }
