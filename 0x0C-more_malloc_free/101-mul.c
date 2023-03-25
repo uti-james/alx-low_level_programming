618af10a0811060c5390bebdfa7ffdc3e399c10b
Uti James <uti.james09@gmail.com>
2023-03-25 12:04:58 +0000

    Wrote a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,130 +1,108 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-#include "main.h"
-
-/**
- * main - entry point
- * @argc: the number of command-line arguments
- * @argv: an array containing the command-line arguments
- *
- * Return: 0 on success, 98 on failure
- */
-int main(int argc, char *argv[])
-{
-    int i, j, len1, len2, res_len, carry, n1, n2, *res;
-
-    if (argc != 3)
-    {
-        _puts("Error");
-        exit(98);
-    }
-    len1 = _strlen(argv[1]);
-    len2 = _strlen(argv[2]);
-    for (i = 0; i < len1; i++)
-    {
-        if (!_isdigit(argv[1][i]))
-        {
-            _puts("Error");
-            exit(98);
-        }
-    }
-    for (i = 0; i < len2; i++)
-    {
-        if (!_isdigit(argv[2][i]))
-        {
-            _puts("Error");
-            exit(98);
-        }
-    }
-    res_len = len1 + len2;
-    res = _calloc(res_len, sizeof(int));
-    if (!res)
-    {
-        _puts("Error");
-        exit(98);
-    }
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        n1 = argv[1][i] - '0';
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            n2 = argv[2][j] - '0';
-            res[i + j + 1] += n1 * n2 + carry;
-            carry = res[i + j + 1] / 10;
-            res[i + j + 1] %= 10;
-        }
-        res[i + j + 1] += carry;
-    }
-    i = 0;
-    while (i < res_len - 1 && res[i] == 0)
-        i++;
-    for (; i < res_len; i++)
-        _putchar(res[i] + '0');
-    _putchar('\n');
-    free(res);
-    return (0);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: the string to measure
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-    int len;
-
-    for (len = 0; s[len]; len++)
-        ;
-    return (len);
-}
 
 /**
  * _isdigit - checks if a character is a digit
  * @c: the character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if the character is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-    return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: the number of elements in the array
- * @size: the size of each element
- *
- * Return: a pointer to the allocated memory, or NULL if it fails
+ * _strlen - gets the length of a string
+ * @s: the string to get the length of
+ * Return: the length of the string
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int _strlen(char *s)
 {
-    char *ptr;
-    unsigned int i;
+	int len = 0;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    ptr = malloc(nmemb * size);
-    if (!ptr)
-        return (NULL);
-    for (i = 0; i < nmemb * size; i++)
-        ptr[i] = 0;
-    return (ptr);
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
 
 /**
- * _puts - writes a string to stdout
- * @str: the string to write
- *
- * Return: nothing
+ * print_error - prints an error message and exits with a status of 98
  */
-void _puts(char *str)
+void print_error(void)
 {
-    int i;
+	char *error_msg = "Error\n";
 
-    for (i = 0; str[i]; i++)
-        _putchar(str[i]);
+	while (*error_msg)
+		_putchar(*(error_msg++));
+
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of pointers to the arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, num1_len, num2_len, result_len;
+	char *num1, *num2, *result;
+
+	if (argc != 3)
+		print_error();
+
+	num1 = argv[1];
+	num2 = argv[2];
+	num1_len = _strlen(num1);
+	num2_len = _strlen(num2);
+
+	for (i = 0; i < num1_len; i++)
+	{
+		if (!_isdigit(num1[i]))
+			print_error();
+	}
+
+	for (j = 0; j < num2_len; j++)
+	{
+		if (!_isdigit(num2[j]))
+			print_error();
+	}
+
+	result_len = num1_len + num2_len;
+	result = malloc(result_len * sizeof(char));
+	if (!result)
+		return (1);
+
+	for (i = 0; i < result_len; i++)
+		result[i] = '0';
+
+	for (i = num1_len - 1; i >= 0; i--)
+	{
+		for (j = num2_len - 1; j >= 0; j--)
+		{
+			int mul = (num1[i] - '0') * (num2[j] - '0');
+			int p1 = i + j;
+			int p2 = i + j + 1;
+			int sum = mul + (result[p2] - '0');
+
+			result[p1] += sum / 10;
+			result[p2] = (sum % 10) + '0';
+		}
+	}
+
+	i = 0;
+	while (result[i] == '0' && i < result_len - 1)
+		i++;
+
+	while (i < result_len)
+		_putchar(result[i++]);
+
+	_putchar('\n');
+
+	free(result);
+
+	return (0);
 }
