47fd5e3b7c7d9ed543b6cd94d8984062ac3fdc43
Uti James <uti.james09@gmail.com>
2023-03-25 02:34:33 +0000

    Added some indentations to make the code more readable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,35 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of its own main function.
- * @argc: the number of arguments passed to the program
- * @argv: an array of pointers to the arguments
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments passed to the program
  *
- * Return: 0 on success, 1 if the number of arguments is incorrect,
- *         2 if the number of bytes is negative
+ * Return: 0 if successful, 1 if incorrect number of 
+ * arguments, 2 if negative number of bytes
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, num_bytes;
+	int bytes, i;
+	char *main_ptr = (char *)main; // get the pointer to the beginning of main function
 
-	if (argc != 2)
+	if (argc != 2) // check if correct number of arguments is passed
 	{
-		printf("Error\n");
-		return (1);
+		printf("Error\n"); // print error message
+		return 1; // return 1 to indicate error
 	}
 
-	num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]); // convert the argument to integer
 
-	if (num_bytes < 0)
+	if (bytes < 0) // check if number of bytes is negative
 	{
-		printf("Error\n");
-		return (2);
+		printf("Error\n"); // print error message
+		return 2; // return 2 to indicate error
 	}
 
-	for (i = 0; i < num_bytes; i++)
-		printf("%02hhx ", *((unsigned char *)main + i));
+	for (i = 0; i < bytes - 1; i++) // loop through the opcodes
+	{
+		printf("%02hhx ", *(main_ptr + i)); // print the opcode in hexadecimal format
+	}
+	printf("%02hhx\n", *(main_ptr + i)); // print the last opcode and newline
 
-	printf("\n");
-	return (0);
+	return 0; // return 0 to indicate success
 }
