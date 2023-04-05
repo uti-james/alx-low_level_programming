a1488a95954f20493563f9f40bf9560d18cc9dce
Uti James <uti.james09@gmail.com>
2023-04-05 07:10:33 +0000

    Reworked the list.h file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string whose length is to be found.
+ *
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+        len++;
+
+    return (len);
+}
 
 /**
  * add_node_end - adds a new node at the end of a list_t list.
@@ -33,21 +52,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	temp_node->next = new_node;
 
 	return (new_node);
-}
-
-/**
- * _strlen - returns the length of a string.
- * @s: string whose length is to be found.
- *
- * Return: length of the string.
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
 }
