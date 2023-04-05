98d90aa9f4160959fe6e190cce58ded7e4e25087
Uti James <uti.james09@gmail.com>
2023-04-05 07:25:57 +0000

    Wrote a function that prints a text before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startupfun - Prints a message before the main function is executed.
+ *
+ * Return: Always void.
+ */
+void startupfun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
