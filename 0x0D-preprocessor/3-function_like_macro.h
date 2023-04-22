c474189459e6f1072ca92062bf63176abb503ae9
uti-james <uti.james09@gmail.com>
2023-04-22 13:20:34 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_FILE_H
-#define HEADER_FILE_H
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif /* HEADER_FILE_H */
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_FILE_H
-#define HEADER_FILE_H
-
-#define SUM(x, y) ((x) + (y))
-
-#endif /* HEADER_FILE_H */
